8011d69553dd45cb04c6b11bb49c5018f4fa5766
Yuri Takeo Miyazaki <yuritakeo@ucl.br>
2024-04-14 16:53:45 -0300

    Atualizando pagina vagas

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,17 @@ import Menu from '../Menu'
 
 import "./style.css"
 
+import buttonMenuHambuerguer from '../../assets/buttonMenuHamburguer.svg'
+
 export default function Header() {
     return (
         <header>
             <div className="faixa-topo-azul" />
 
             <div className="container-header">
+                <div className='buttonMenuHamburguer'>
+                    <img src={buttonMenuHambuerguer} alt="botão menu" />
+                </div>
                 <LogoEmpresa />
                 <Menu />
             </div>
--- src/pages/Vagas/index.jsx
+++ src/pages/Vagas/index.jsx
@@ -3,12 +3,16 @@ import { useState } from "react"
 import iconVagas from '../../assets/icon-vagas.svg'
 import iconLupa from '../../assets/lupa.svg'
 
+
+import logops from '../../assets/logops.svg'
+import logovr from '../../assets/logovr.svg'
+
 import './style.css'
 export default function Vagas() {
     const [vagas, setVagas] = useState(
         [
             {
-                id: 1, empresa: "Praia Sol", area: "Administrativo", funcao: "Auxiliar de operação", nomeVaga: "Bilhetagem Eletrónica", status: "Aberto",
+                id: 1, empresa: "Praia Sol", empresaLogo: logops, area: "Administrativo", funcao: "Auxiliar de operação", nomeVaga: "Bilhetagem Eletrónica", status: "Aberto",
                 requisitos:
                     [
                         "Informática Básica",
@@ -17,7 +21,7 @@ export default function Vagas() {
                     ]
             },
             {
-                id: 2, empresa: "Praia Sol", area: "Manutenção", funcao: "Servente", nomeVaga: "Servente Interno", status: "Aberto",
+                id: 2, empresa: "Praia Sol", empresaLogo: logops, area: "Manutenção", funcao: "Servente", nomeVaga: "Servente Interno", status: "Aberto",
                 requisitos:
                     [
                         "Aptidão Física",
@@ -25,7 +29,7 @@ export default function Vagas() {
                     ]
             },
             {
-                id: 3, empresa: "Vereda", area: "Manuntenção", funcao: "Servente", nomeVaga: "Servente Pátio", status: "Aberto",
+                id: 3, empresa: "Vereda", empresaLogo: logovr, area: "Manuntenção", funcao: "Servente", nomeVaga: "Servente Pátio", status: "Aberto",
                 requisitos:
                     [
                         "Aptidão Física",
@@ -33,7 +37,7 @@ export default function Vagas() {
                     ]
             },
             {
-                id: 4, empresa: "Praia Sol", area: "Operacional", funcao: "Motorista", nomeVaga: "Motorista", status: "Aberto",
+                id: 4, empresa: "Praia Sol", empresaLogo: logops, area: "Operacional", funcao: "Motorista", nomeVaga: "Motorista", status: "Aberto",
                 requisitos:
                     [
                         "Habilitação D ou E",
@@ -44,14 +48,16 @@ export default function Vagas() {
 
     return (
         <>
-            <h1 id="titulo">Trabalhe Conosco</h1>
+            <div className="container">
+                <h1 id="titulo">Trabalhe Conosco</h1>
 
-            <div className="trabalhe-conosco">
-                <img src={iconVagas} alt="Icone da image de vagas" />
-                <h2>Quer trabalhar conosco?</h2>
+                <div className="trabalhe-conosco">
+                    <img src={iconVagas} alt="Icone da image de vagas" />
+                    <h2>Quer trabalhar conosco?</h2>
+                </div>
             </div>
 
-            <div className="form-nossa-oportunidades">
+            <div className="container form-nossa-oportunidades">
                 <h3>NOSSAS OPORTUNIDADES HOJE</h3>
                 <div className="container-campo-pesquisa">
                     <input id="pesquisa-vaga" type="text" placeholder="Digite o nome da vaga" />
@@ -59,7 +65,7 @@ export default function Vagas() {
                 </div>
                 <div className="forms-filtro-vagas">
                     <p>Tipo de Vaga</p>
-                    <select name="tipoVaga" id="tipoVaga">
+                    <select className="item-select" name="tipoVaga" id="tipoVaga">
                         <option value="todas">Todas</option>
                         <option value="efetivo">Efetivo</option>
                         <option value="estagiario">Estágiario</option>
@@ -68,7 +74,7 @@ export default function Vagas() {
 
 
                     <p>Empresa</p>
-                    <select name="empresa" id="empresa">
+                    <select className="item-select" name="empresa" id="empresa">
                         <option value="todas">Todas</option>
                         <option value="praia-sol">Praia Sol</option>
                         <option value="vereda">Vereda</option>
@@ -76,7 +82,7 @@ export default function Vagas() {
 
 
                     <p>Áreas</p>
-                    <select name="area" id="area">
+                    <select className="item-select" name="area" id="area">
                         <option value="todas">Todas</option>
                         <option value="operacional">Operacional</option>
                         <option value="administrativo">Administrativo</option>
@@ -85,18 +91,23 @@ export default function Vagas() {
                 </div>
             </div>
 
-            <div className="listagem-vagas">
+            <div className="container listagem-vagas">
+                <h1>Vagas</h1>
                 {
                     vagas.map((vaga) => {
                         return <div className="item-lista">
                             <div className="conteudo-vaga">
-                                <p><sgrong>Função:</sgrong> {vaga.funcao}</p>
-                                <p><sgrong>Vaga para:</sgrong> {vaga.nomeVaga}</p>
-                                <p><sgrong>Empresa:</sgrong> {vaga.empresa}</p>
+                                <img src={vaga.empresaLogo} alt="" />
+                                <p><sgrong>{vaga.funcao}</sgrong></p>
+                                <p><sgrong>Requisitos:</sgrong> {vaga.requisitos.map((valor)=>{
+                                    return <p>{valor}</p>
+                                })}</p>
+
+                                <div className="botao-candidatar">
+                                    <button>Inscrever-se</button>
+                                </div>
                             </div>
-                            <div className="botao-candidatar">
-                                <button>Inscrever-se</button>
-                            </div>
+
                         </div>
                     })
                 }
